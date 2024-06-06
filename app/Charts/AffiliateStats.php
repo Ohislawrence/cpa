e90b7ce4f18ad432db05ea87908b062e67dfd514
Ohislawrence <lawrenceohis@gmail.com>
2024-06-06 22:39:29 +0100

    chart worked on

--- app/Charts/AffiliateStats.php
+++ app/Charts/AffiliateStats.php
@@ -4,7 +4,10 @@ namespace App\Charts;
 
 use App\Models\Click;
 use ArielMejiaDev\LarapexCharts\LarapexChart;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AffiliateStats
 {
@@ -17,11 +20,25 @@ class AffiliateStats
 
     public function build(): \ArielMejiaDev\LarapexCharts\LineChart
     {
+        $days = CarbonPeriod::create(now()->subDay(7), now());
+        $daycs =[];
+            foreach ($days as $day) {
+                $daycs[] = $day->format('d-m-Y');
+            }
+
+        $clicks = Click::where('user_id', Auth::user()->id)->groupBy(DB::raw("DATE_FORMAT(created_at, '%d-%m-%Y')"))->orderBy('created_at')->get();
+        //dd($clicks->clickID);
+        foreach($clicks as $click)
+        {
+            
+            $cl[] = $click->count();
+        }
+        
         return $this->chart->lineChart()
             ->setTitle('Sales during 2021.')
             ->setSubtitle('Physical sales vs Digital sales.')
-            ->addData('Physical sales', [Click::where('user_id', Auth::user()->id)->count()])
-            ->addData('Digital sales', [70, 29, 77, 28, 55, 45])
-            ->setXAxis(['January', 'February', 'March', 'April', 'May', 'June']);
+            ->addData('Clicks', $cl)
+            //->addData('Digital sales', [70, 29, 77, 28, 55, 45])
+            ->setXAxis($daycs);
     }
 }

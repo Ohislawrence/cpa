769808c8d43078e24ce4360ecebf4c75ce2a8295
Ohis Lawrence <49398616+Ohislawrence@users.noreply.github.com>
2024-06-18 12:04:09 -0700

    vite removed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        
 		@yield('header')
 		<link href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" rel="stylesheet">
 <link href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" rel="stylesheet">

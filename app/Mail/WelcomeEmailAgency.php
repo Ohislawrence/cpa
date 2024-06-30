8658d4bec17452ce31f7b17df5078308aa151ca0
Ohislawrence <lawrenceohis@gmail.com>
2024-06-30 15:05:50 +0100

    agency sign up done2

--- app/Mail/WelcomeEmailAgency.php
+++ app/Mail/WelcomeEmailAgency.php
@@ -39,7 +39,7 @@ class WelcomeEmailAgency extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.welcomeEmailAgency',
         );
     }
 
